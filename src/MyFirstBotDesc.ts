36da7f964e7aab2a44c9c8deb4fa9fcdd167573d
Vanes <vanes.parma@gmail.com>
2017-09-14 11:28:48 +0200

    bot.on(message)

--- src/MyFirstBotDesc.ts
+++ src/MyFirstBotDesc.ts
@@ -78,7 +78,8 @@ export function start(_bot, done)  {
       linee = data
 		//TODO: Effetto collaterale !!!!!
         calcNumeriLinea(linee)
-		//TODO: Effetto collaterale !!!!!
+        //TODO: Effetto collaterale !!!!!
+                /*
 		const hearings : any[] = [
 			{ "tokens":["a","b"], "action":  ab_action },
 			{
@@ -91,7 +92,7 @@ export function start(_bot, done)  {
     		}
 		]   		
 
-        /*
+
 		for (let h of hearings){
 			_bot && h.convoaction && _bot.hear(h.tokens, (payload, chat) => {
 				chat.conversation(convo => h.convoaction(convo, payload.message.text)) 
@@ -103,108 +104,153 @@ export function start(_bot, done)  {
 			console.log("** convo hearing for "+h.tokens.toString())
 		}*/
 
-		/*
+		
+        
         // bot && bot.hear("", (payload, chat) => {   })
         bot && bot.on('message', (payload, chat, data) => {
             const text = payload.message.text;
             if (data.captured) { return; }
             processMessage(chat, text)
         });
-*/
-		const linea_numLinea_regexp = /(?:linea)?(?:[0-9]+)/i
+        bot && bot.on('postback:ORARI_ASC', (payload, chat, data) => {
+            if (data.captured) { return; }
+            onOrarioLinea(chat, payload, data, 'A')
+        });
+        bot && bot.on('postback:ORARI_DESC', (payload, chat, data) => {
+            if (data.captured) { return; }
+            onOrarioLinea(chat, payload, data, 'D')
+        });
+
+        /*
         bot && bot.hear(linea_numLinea_regexp, (payload, chat) => {
             const text = payload.message.text;
-            const numLinea=  /[0-9]+/.exec(text) // text.replace(testNumberSomewhere, '$2')
+            const numLinea=  /(?:\b[0-9]+\b|1A|1B|5A|96A)/.exec(text) // text.replace(testNumberSomewhere, '$2')
             if (lineeMap.has(numLinea)) {
                 const linee = lineeMap.get(numLinea)
                 onNumLinea(chat, linee)
             }
             else
                 chat.say("Non conosco la linea "+numLinea)        });
-		
+		*/
 		done(data)
 	})
 }    
 
 const processMessage = (chat,text) => {
-    const testNumberAtStart = /(^\d+)(.+$)/i             // $1
-    const testNumberSomewhere = /(^.+)(\w\d+\w)(.+$)/i   // $2
+//    const testNumberAtStart = /(^\d+)(.+$)/i             // $1
+//    const testNumberSomewhere = /(^.+)(\w\d+\w)(.+$)/i   // $2
+    const linea_numLinea_regexp = /(?<=linea )(\b[0-9]+\b|1A|1B|5A|96A)$/i
+
+    console.log("VP>on message :"+text)
     
-//    if ( /\blinea\b/.test(text) ) {
-      if ( /^[0-9]+$/.test(text) ) {
-            const numLinea= text // text.replace(testNumberSomewhere, '$2')
-            if (lineeMap.has(numLinea)) {
-                const linee = lineeMap.get(numLinea)
-                onNumLinea(chat, linee)
-            }
-            else
-                chat.say("Non conosco la linea "+numLinea)
-        } 
+    let match = linea_numLinea_regexp.exec(text)
+    if (match && match[1]) {
+        const numLinea = match[1]
+        console.log("   -- numLinea"+numLinea)
+        if (lineeMap.has(numLinea)) {
+            const linee = lineeMap.get(numLinea)
+            onNumLinea(chat, linee)
+        }
+        else
+            chat.say("Non conosco la linea "+numLinea)
+
+        return;
+    }
+
+    chat.say("Non ho capito. Prova a ripetere")
 }
 
 
-const onNumLinea = (chat, linee:any[]) => {
+const onNumLinea = (chat, linee:any[]) : void => {
     if (linee.length===1)
-        numlineaUnivoci_actionGeneric(chat, linee[0])
+        onLinea_usaGeneric(chat, linee[0])
+    //    onLinea_usaConvo(chat, linee[0])
 }
 
-const ab_action = (convo, heard:string) : void => {
-        convo.say(`Hai detto a o b : ${heard}`);
-		convo.end();
-    }
-const numlineaUnivoci_action = (convo, numLinea:string) : void => {
-        const msgs = _messagesLinea(numLinea)
-        
-        convo.say(msgs[0]).then(()=>{
-            convo.say(msgs[1]).then(()=>{
-                convo.say(msgs[2]).then(()=>{
-                    convo.say(msgs[3])
-                    convo.end()
-                })
-            })
-        })
-    }
-const numlineaRipetuti_action = (convo, heard:string) : void => {
-    convo.say(`Linea ripetuta : ${heard}`)
-    convo.end();
-}
+const _orariButtons = (atext, dtext, url) : any[] => [
+    {
+      "type":"postback",
+      "title":"Orari verso "+atext,
+      "payload":"ORARI_ASC"
+    },              
+    {
+     "type":"postback",
+     "title":"Orari verso "+dtext,
+     "payload":"ORARI_DESC"
+    } ,
+   {
+     "type":"web_url",
+     "url": url || "http://www.startromagna.it",
+     "title":"Sito"
+   }              
+ ]
 
-
-const numlineaUnivoci_actionGeneric = (chat, linea:any) : void => {
+const onLinea_usaGeneric = (chat, linea:any) : void => {
     // const linea = lineeMap.get(numLinea)[0]
-    const elements = [
+    const urlLinea = `http://servizi.startromagna.it/opendata/od/ui/tpl/${linea.Bacino}/linee/${linea.LINEA_ID}`
+    chat.sendGenericTemplate( [
         {
-         "title":"Linea "+linea.display_name,
+         "title": linea.name || ("Linea "+linea.display_name),
 //             "image_url":"https://petersfancybrownhats.com/company_image.png",
-         "subtitle": linea.strip_asc_direction+"\n"+linea.strip_desc_direction,
+//         "subtitle": linea.strip_asc_direction+"\n"+linea.strip_desc_direction,
+         "subtitle": linea.strip_asc_direction,
          "default_action": {
            "type": "web_url",
-           "url": "http://www.startromagna.it",
+           "url": urlLinea,
 //               "messenger_extensions": true,
 //               "webview_height_ratio": "tall",
 //               "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
          },
-         "buttons":[
-           {
-             "type":"postback",
-             "title":"Orari verso "+linea.strip_asc_direction,
-             "payload":"DEVELOPER_DEFINED_PAYLOAD"
-           },              
-           {
-            "type":"postback",
-            "title":"Orari verso "+linea.strip_desc_direction,
-            "payload":"DEVELOPER_DEFINED_PAYLOADR"
-          },
-          {
-            "type":"web_url",
-            "url": "http://www.startromagna.it",
-            "title":"Sito"
-          }              
-        ]      
+
+         "buttons": _orariButtons(linea.strip_asc_direction, linea.strip_desc_direction, urlLinea),
        }
-     ]
-    chat.sendGenericTemplate(elements)        
+     ])
 }
+
+const onOrarioLinea = (chat, payload, data, AorD) => {
+    console.log(JSON.stringify(payload))
+    console.log(JSON.stringify(data))
+}
+
+    const onLinea_usaConvo = (chat, linea:any) : void => {
+        chat.conversation(convo => {
+            convo.ask(
+            // question :
+            { 
+              text: 'Favorite color?',
+              quickReplies: [ 'Red', 'Blue', 'Green' ]
+            }, 
+            // answer :
+            (payload, convo) => {
+              const text = payload.message.text;
+              convo.say(`Oh your favorite color is ${text}, cool!`);
+              convo.end();
+            }, 
+            // callbacks :
+            [
+              {
+                event: 'quick_reply',
+                callback: (payload, convo) => {
+                  const text = payload.message.text;
+                  convo.say(`Thanks for choosing one of the options. Your favorite color is ${text}`);
+                  convo.end();
+                }
+              }
+            ]
+            // options: (options di chat.say che chiama bot.sendMessge ) è solo per il typing indicator:
+            /*
+                if (options && options.typing) {
+                    const autoTimeout = (message && message.text) ? message.text.length * 10 : 1000;
+                    const timeout = (typeof options.typing === 'number') ? options.typing : autoTimeout;
+                    return this.sendTypingIndicator(recipientId, timeout).then(req);
+                }        
+            */
+            );
+          });    
+        
+
+}
+
 
 
 
@@ -217,4 +263,27 @@ const _messagesLinea = (numLinea:string, index:number=0) : string[] => {
     msgs.push(linea.desc_direction+'\n'+linea.desc_note)
     
     return msgs;
+}
+
+
+
+const ab_action = (convo, heard:string) : void => {
+    convo.say(`Hai detto a o b : ${heard}`);
+    convo.end();
+}
+const numlineaUnivoci_action = (convo, numLinea:string) : void => {
+    const msgs = _messagesLinea(numLinea)
+    
+    convo.say(msgs[0]).then(()=>{
+        convo.say(msgs[1]).then(()=>{
+            convo.say(msgs[2]).then(()=>{
+                convo.say(msgs[3])
+                convo.end()
+            })
+        })
+    })
+}
+const numlineaRipetuti_action = (convo, heard:string) : void => {
+convo.say(`Linea ripetuta : ${heard}`)
+convo.end();
 }
