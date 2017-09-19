4ad85aa72743ce7cd9c70e80d43b58a2f9db7f73
Vanes <vanes.parma@gmail.com>
2017-09-19 17:33:40 +0200

    location

--- dist/skills/location.js
+++ dist/skills/location.js
@@ -31,27 +31,29 @@ exports.onLocationReceived = (chat, coords) => {
         let dist = 9e6;
         let nearestStop;
         // These two queries will run sequentially.
-        db.each("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function (err, rows) {
-            rows.forEach(function (row) {
-                let d = utils.distance(coords.lat, coords.long, row.stop_lat, row.stop_lon);
-                if (d < dist) {
-                    dist = d;
-                    nearestStop = row;
-                }
-            });
-        }); // end run
+        db.each("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function (err, row) {
+            //rows.forEach(row => {
+            let d = utils.distance(coords.lat, coords.long, row.stop_lat, row.stop_lon);
+            if (d < dist) {
+                dist = d;
+                nearestStop = row;
+            }
+            //}); // end forEach
+        }); // end each
         var q = "SELECT a.route_id FROM trips a WHERE a.trip_id IN (SELECT b.trip_id FROM stop_times b WHERE b.stop_id='" + nearestStop.stop_id + "') GROUP BY a.route_id";
-        db.each(q, function (err, rows) {
-            //        callback(nearestStop, dist);
+        let lineePassanti = [];
+        db.each(q, function (err, row) {
+            lineePassanti.push(row.route_id);
+        }, function () {
             chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist.toFixed(0)} metri in linea d'aria`)
                 .then(() => {
                 chat.say({
                     text: 'Ci passano queste linee:',
-                    quickReplies: rows.map(r => r.route_id),
+                    quickReplies: lineePassanti,
                 });
             });
-            db.close();
         }); // end run 
+        db.close();
     });
 };
 //# sourceMappingURL=location.js.map
--- src/skills/location.ts
+++ src/skills/location.ts
@@ -39,29 +39,32 @@ export const onLocationReceived = (chat, coords) => {
         let nearestStop;
 
     // These two queries will run sequentially.
-        db.each("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function (err, rows) {
-            rows.forEach(function (row) {
-                let d = utils.distance(coords.lat, coords.long, row.stop_lat, row.stop_lon)
-                if (d < dist) {
-                    dist = d;
-                    nearestStop = row;
-                }
-            });
-        }); // end run
+        db.each("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function (err, row) {
+            //rows.forEach(row => {
+                let d = utils.distance(coords.lat, coords.long, row.stop_lat, row.stop_lon);
+                if (d < dist) { dist = d;  nearestStop = row; }
+            //}); // end forEach
+        }); // end each
+
         var q = "SELECT a.route_id FROM trips a WHERE a.trip_id IN (SELECT b.trip_id FROM stop_times b WHERE b.stop_id='"+nearestStop.stop_id+"') GROUP BY a.route_id"
-        db.each(q, function (err, rows) {
-            //        callback(nearestStop, dist);
-            chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist.toFixed(0)} metri in linea d'aria`)
-            .then(()=>{
-                chat.say({
-                    text: 'Ci passano queste linee:',
-                    quickReplies : rows.map(r=>r.route_id),
-                })
-            });
-            db.close();
+        let lineePassanti = []
+        db.each(q, 
+            function (err, row) { // chiamata per ogni riga
+                lineePassanti.push(row.route_id)
+            },
+            function () { // chiamata al completamento
+                chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist.toFixed(0)} metri in linea d'aria`)
+                .then(()=>{
+                    chat.say({
+                        text: 'Ci passano queste linee:',
+                        quickReplies : lineePassanti,
+                    })
+                });
+            }
     
-        });// end run 
-            
+        );// end run 
+        db.close();
+        
       });
     
     }
