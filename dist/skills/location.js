83a9621b3625c252f4e7566400710554e82e8ebb
Vanes <vanes.parma@gmail.com>
2017-09-19 17:19:22 +0200

    location

--- dist/skills/location.js
+++ dist/skills/location.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils = require("../utils");
 var sqlite3 = require('sqlite3').verbose();
-exports.onLocationReceived = (chat, coords) => {
+exports.onLocationReceivedOLD = (chat, coords) => {
     var db = new sqlite3.Database('dist/db/database.sqlite3');
     db.all("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function (err, rows) {
         let dist = 9e6;
@@ -14,9 +14,44 @@ exports.onLocationReceived = (chat, coords) => {
                 nearestStop = row;
             }
         });
-        //        callback(nearestStop, dist);
-        chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist} metri`);
         db.close();
+        //        callback(nearestStop, dist);
+        chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist.toFixed(0)} metri in linea d'aria`)
+            .then(() => {
+            chat.say({
+                text: 'Ci passano le linee',
+                quickReplies: [],
+            });
+        });
+    });
+};
+exports.onLocationReceived = (chat, coords) => {
+    var db = new sqlite3.Database('dist/db/database.sqlite3');
+    db.serialize(function () {
+        let dist = 9e6;
+        let nearestStop;
+        // These two queries will run sequentially.
+        db.each("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function (err, rows) {
+            rows.forEach(function (row) {
+                let d = utils.distance(coords.lat, coords.long, row.stop_lat, row.stop_lon);
+                if (d < dist) {
+                    dist = d;
+                    nearestStop = row;
+                }
+            });
+        }); // end run
+        var q = "SELECT a.route_id FROM trips a WHERE a.trip_id IN (SELECT b.trip_id FROM stop_times b WHERE b.stop_id='" + nearestStop.stop_id + "') GROUP BY a.route_id";
+        db.each(q, function (err, rows) {
+            //        callback(nearestStop, dist);
+            chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist.toFixed(0)} metri in linea d'aria`)
+                .then(() => {
+                chat.say({
+                    text: 'Ci passano queste linee:',
+                    quickReplies: rows.map(r => r.route_id),
+                });
+            });
+            db.close();
+        }); // end run 
     });
 };
 //# sourceMappingURL=location.js.map
--- src/skills/location.ts
+++ src/skills/location.ts
@@ -5,21 +5,64 @@ import service = require("../service")
 
 var sqlite3 = require('sqlite3').verbose();
 
-export const onLocationReceived = (chat, coords) => {
+export const onLocationReceivedOLD = (chat, coords) => {
     var db = new sqlite3.Database('dist/db/database.sqlite3');
-    db.all("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function(err, rows) {
-        let dist = 9e6
+    db.all("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function (err, rows) {
+        let dist: number = 9e6
         let nearestStop
         rows.forEach(function (row) {
-            let d = utils.distance(coords.lat,coords.long,row.stop_lat,row.stop_lon)
-            if (d<dist) {
-                dist=d;
+            let d = utils.distance(coords.lat, coords.long, row.stop_lat, row.stop_lon)
+            if (d < dist) {
+                dist = d;
                 nearestStop = row;
             }
         });
-//        callback(nearestStop, dist);
-        chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist} metri`)
+
+
         db.close();
-    });
+
+        //        callback(nearestStop, dist);
+        chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist.toFixed(0)} metri in linea d'aria`)
+        .then(()=>{
+            chat.say({
+                text: 'Ci passano le linee',
+                quickReplies : [],
+            })
+        });
+    })
 }
+
+export const onLocationReceived = (chat, coords) => {
+    var db = new sqlite3.Database('dist/db/database.sqlite3');
+    db.serialize(function() {
+        let dist: number = 9e6
+        let nearestStop;
+
+    // These two queries will run sequentially.
+        db.each("SELECT stop_id,stop_name,stop_lat,stop_lon FROM stops", function (err, rows) {
+            rows.forEach(function (row) {
+                let d = utils.distance(coords.lat, coords.long, row.stop_lat, row.stop_lon)
+                if (d < dist) {
+                    dist = d;
+                    nearestStop = row;
+                }
+            });
+        }); // end run
+        var q = "SELECT a.route_id FROM trips a WHERE a.trip_id IN (SELECT b.trip_id FROM stop_times b WHERE b.stop_id='"+nearestStop.stop_id+"') GROUP BY a.route_id"
+        db.each(q, function (err, rows) {
+            //        callback(nearestStop, dist);
+            chat.say(`La fermata più vicina è ${nearestStop.stop_name} a ${dist.toFixed(0)} metri in linea d'aria`)
+            .then(()=>{
+                chat.say({
+                    text: 'Ci passano queste linee:',
+                    quickReplies : rows.map(r=>r.route_id),
+                })
+            });
+            db.close();
+    
+        });// end run 
+            
+      });
+    
+    }
 
